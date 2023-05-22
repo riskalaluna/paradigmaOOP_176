a1d9438ab19e0fb3ed94efb28595ae39dd3dcb62
riskalaluna <riskalaluna@gmail.com>
2023-05-22 15:45:56 +0700

    10

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -6,4 +6,11 @@ class ibu {
 public:
 	string nama;
 	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
 };
