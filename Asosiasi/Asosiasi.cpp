08e995e4840706f6bd9aa46a3f705bc37f4a036f
riskalaluna <riskalaluna@gmail.com>
2023-05-22 17:37:28 +0700

    class dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -11,6 +11,24 @@ public:
 		cout << "Pasien \"" << nama << "\" ada\n";
 	}
 	~pasien() {
-
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
 	}
+	void tambahDokter(dokter*);
+	void cetakDokter();
+};
+
+class dokter {
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada\n";
+	}
+
+	void tambahPasien(pasien*);
+	void cetakPasien();
 };
